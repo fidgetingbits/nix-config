816b57a59a2496d6b40ecdbc98f663d5f0a28071
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-03-11 16:57:32 +0800

    fix: allow usb sniffing and switch to unstable wireshark

--- hosts/common/optional/wireshark.nix
+++ hosts/common/optional/wireshark.nix
@@ -1,8 +1,19 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
-  programs.wireshark = {
-    enable = true;
-  };
+
+  boot.kernelModules = [ "usbmon" ];
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usbmon", GROUP="wireshark", MODE="640"
+  '';
+
+  #programs.wireshark = {
+  #  enable = true;
+  #  package = pkgs.wireshark-qt;
+  #};
+
+  environment.systemPackages = [
+    pkgs.unstable.wireshark # 4.2.x is broken with latest QT
+  ];
 
   users.users.${config.hostSpec.username} = {
     extraGroups = [ "wireshark" ];
