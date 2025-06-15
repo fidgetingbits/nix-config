c184b500a072ce2ab418b032dcc986055b3d16cb
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-06-15 09:22:08 +0800

    fix: add font cache refresh on home activation

--- home/common/core/nixos.nix
+++ home/common/core/nixos.nix
@@ -34,6 +34,13 @@
           ;
       }
     );
+    # Reload font cache on rebuild to avoid issues similar to
+    # https://www.reddit.com/r/NixOS/comments/1kwogzf/after_moving_to_2505_system_fonts_no_longer/
+    activation.reloadFontCache = lib.hm.dag.entryAfter [ "linkActivation" ] ''
+      if [ -x "${pkgs.fontconfig}/bin/fc-cache" ]; then
+        ${pkgs.fontconfig}/bin/fc-cache -f
+      fi
+    '';
   };
 
   services.ssh-agent.enable = true;
