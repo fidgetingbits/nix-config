131f63fa263df7826372741be98dbbbf40214e0c
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-06-23 10:12:34 +0800

    feat(builder): add root ssh config for remote builder

--- hosts/common/optional/distributed-builds.nix
+++ hosts/common/optional/distributed-builds.nix
@@ -1,4 +1,7 @@
 {
+  inputs,
+  lib,
+  config,
   pkgs,
   ...
 }:
@@ -26,4 +29,12 @@
       path = "/root/.ssh/id_builder";
     };
   };
+}
+// lib.optionalAttrs (inputs ? "home-manager") {
+
+  home-manager.users.root.home.file.".ssh/config".text = ''
+    Host oppo
+      HostName oppo.${config.hostSpec.domain}
+      Port ${builtins.toString config.hostSpec.networking.ports.tcp.ssh}
+  '';
 }
