568c7f8b6fc4a2bd6c0b44349753e56a6fb0d585
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-07-02 22:03:52 +0800

    refactor(hostSpec): switch to using freeformType option

--- modules/common/host-spec.nix
+++ modules/common/host-spec.nix
@@ -7,138 +7,144 @@
   ...
 }:
 {
-  options.hostSpec = {
-    # Data variables that don't dictate configuration settings
-    username = lib.mkOption {
-      type = lib.types.str;
-      description = "The username of the host";
-    };
-    hostName = lib.mkOption {
-      type = lib.types.str;
-      description = "The hostname of the host";
-    };
-    email = lib.mkOption {
-      type = lib.types.attrsOf lib.types.str;
-      description = "The email of the user";
-    };
-    work = lib.mkOption {
-      default = { };
-      type = lib.types.attrsOf lib.types.anything;
-      description = "An attribute set of work-related information if isWork is true";
-    };
-    networking = lib.mkOption {
-      default = { };
-      type = lib.types.attrsOf lib.types.anything;
-      description = "An attribute set of networking information";
-    };
-    wifi = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Used to indicate if a host has wifi";
-    };
-    domain = lib.mkOption {
-      type = lib.types.str;
-      default = "local"; # Need a default for the installer
-      description = "The domain of the host";
-    };
-    userFullName = lib.mkOption {
-      type = lib.types.str;
-      description = "The full name of the user";
-    };
-    handle = lib.mkOption {
-      type = lib.types.str;
-      description = "The handle of the user (eg: github user)";
-    };
-    home = lib.mkOption {
-      type = lib.types.str;
-      description = "The home directory of the user";
-      default =
-        let
-          user = config.hostSpec.username;
-        in
-        if pkgs.stdenv.isLinux then "/home/${user}" else "/Users/${user}";
-    };
-    persistFolder = lib.mkOption {
-      type = lib.types.str;
-      description = "The folder to persist data if impermenance is enabled";
-      default = "";
-    };
+  options.hostSpec = lib.mkOption {
+    type = lib.types.submodule {
+      freeformType = with lib.types; attrsOf str;
 
-    # Configuration Settings
-    isMinimal = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Indicate a minimal host";
-    };
-    isProduction = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = "Indicate a production host";
-    };
-    isServer = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Indicate a server host";
-    };
-    isWork = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Indicate a host that uses work resources";
-    };
-    isDevelopment = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Indicate a host used for development";
-    };
-    useYubikey = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Indicate if the host uses a yubikey";
-    };
-    voiceCoding = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Indicate a host that uses voice coding";
-    };
-    isAutoStyled = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Indicate a host that wants auto styling like stylix";
-    };
-    useNeovimTerminal = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Indicate a host that uses neovim for terminals";
-    };
-    useWindowManager = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = "Indicate a host that uses a window manager";
-    };
-    useAtticCache = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = "Indicate a host that uses LAN atticd for caching";
-    };
-    hdr = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Indicate a host that uses HDR";
-    };
-    scaling = lib.mkOption {
-      type = lib.types.str;
-      default = "1";
-      description = "Indicate what scaling to use. Floating point number";
-    };
-    wallpaper = lib.mkOption {
-      type = lib.types.path;
-      default = "${inputs.nix-assets}/images/wallpapers/jigglypuff.webp";
-      description = "Path to wallpaper to use for system";
-    };
-    useWayland = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Indicate a host that uses Wayland";
+      options = {
+        # Data variables that don't dictate configuration settings
+        username = lib.mkOption {
+          type = lib.types.str;
+          description = "The username of the host";
+        };
+        hostName = lib.mkOption {
+          type = lib.types.str;
+          description = "The hostname of the host";
+        };
+        email = lib.mkOption {
+          type = lib.types.attrsOf lib.types.str;
+          description = "The email of the user";
+        };
+        work = lib.mkOption {
+          default = { };
+          type = lib.types.attrsOf lib.types.anything;
+          description = "An attribute set of work-related information if isWork is true";
+        };
+        networking = lib.mkOption {
+          default = { };
+          type = lib.types.attrsOf lib.types.anything;
+          description = "An attribute set of networking information";
+        };
+        wifi = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description = "Used to indicate if a host has wifi";
+        };
+        domain = lib.mkOption {
+          type = lib.types.str;
+          default = "local"; # Need a default for the installer
+          description = "The domain of the host";
+        };
+        userFullName = lib.mkOption {
+          type = lib.types.str;
+          description = "The full name of the user";
+        };
+        handle = lib.mkOption {
+          type = lib.types.str;
+          description = "The handle of the user (eg: github user)";
+        };
+        home = lib.mkOption {
+          type = lib.types.str;
+          description = "The home directory of the user";
+          default =
+            let
+              user = config.hostSpec.username;
+            in
+            if pkgs.stdenv.isLinux then "/home/${user}" else "/Users/${user}";
+        };
+        persistFolder = lib.mkOption {
+          type = lib.types.str;
+          description = "The folder to persist data if impermenance is enabled";
+          default = "";
+        };
+
+        # Configuration Settings
+        isMinimal = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description = "Indicate a minimal host";
+        };
+        isProduction = lib.mkOption {
+          type = lib.types.bool;
+          default = true;
+          description = "Indicate a production host";
+        };
+        isServer = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description = "Indicate a server host";
+        };
+        isWork = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description = "Indicate a host that uses work resources";
+        };
+        isDevelopment = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description = "Indicate a host used for development";
+        };
+        useYubikey = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description = "Indicate if the host uses a yubikey";
+        };
+        voiceCoding = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description = "Indicate a host that uses voice coding";
+        };
+        isAutoStyled = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description = "Indicate a host that wants auto styling like stylix";
+        };
+        useNeovimTerminal = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description = "Indicate a host that uses neovim for terminals";
+        };
+        useWindowManager = lib.mkOption {
+          type = lib.types.bool;
+          default = true;
+          description = "Indicate a host that uses a window manager";
+        };
+        useAtticCache = lib.mkOption {
+          type = lib.types.bool;
+          default = true;
+          description = "Indicate a host that uses LAN atticd for caching";
+        };
+        hdr = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description = "Indicate a host that uses HDR";
+        };
+        scaling = lib.mkOption {
+          type = lib.types.str;
+          default = "1";
+          description = "Indicate what scaling to use. Floating point number";
+        };
+        wallpaper = lib.mkOption {
+          type = lib.types.path;
+          default = "${inputs.nix-assets}/images/wallpapers/jigglypuff.webp";
+          description = "Path to wallpaper to use for system";
+        };
+        useWayland = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description = "Indicate a host that uses Wayland";
+        };
+      };
     };
   };
 
