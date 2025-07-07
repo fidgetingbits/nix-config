90e2601f1353fe042f5398ec3d78336c232fc4bb
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-07-07 11:14:35 +0800

    fix(ghostty): workaround linux kernel 6.5.14 io_uring performance regression

--- home/common/optional/ghostty.nix
+++ home/common/optional/ghostty.nix
@@ -1,4 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  programs.ghostty.enable = true;
+  programs.ghostty = {
+    enable = true;
+    # Relevant until linux kernel 6.15.5 or later
+    # See: https://github.com/NixOS/nixpkgs/issues/421442
+    package = pkgs.ghostty.overrideAttrs (_: {
+      preBuild = ''
+        shopt -s globstar
+        sed -i 's/^const xev = @import("xev");$/const xev = @import("xev").Epoll;/' **/*.zig
+        shopt -u globstar
+      '';
+    });
+  };
 }
