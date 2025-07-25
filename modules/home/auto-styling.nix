e89c59b8ae4b490c692c58eef750735c08f4d5f1
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-07-25 11:32:12 +0800

    refactor: re-enable zellij stylix theme

--- modules/home/auto-styling.nix
+++ modules/home/auto-styling.nix
@@ -2,6 +2,6 @@
 {
   config = lib.mkIf config.hostSpec.isAutoStyled {
     # Defined here because zellij is core, but stylix isn't so can infinite recurse
-    #stylix.targets.zellij.enable = true;
+    stylix.targets.zellij.enable = true;
   };
 }
