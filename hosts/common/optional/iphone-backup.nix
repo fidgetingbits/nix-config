5caaa49c02c1a6ce801c2e1e70e4b4bbb023f6c9
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-09-10 08:05:32 +0800

    fix: use different iphone backup package

--- hosts/common/optional/iphone-backup.nix
+++ hosts/common/optional/iphone-backup.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 {
   # Read iphones (see also ifuse?) https://nixos.wiki/wiki/IOS
-  services.usbmuxd.enable = true;
+  services.usbmuxd = {
+    enable = true;
+    package = pkgs.usbmuxd2; # Failing to connect to iphone 15
+  };
   environment.systemPackages = builtins.attrValues { inherit (pkgs) ifuse libimobiledevice; };
 }
