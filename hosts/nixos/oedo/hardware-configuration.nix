714ba41e9b600183afa69452a575600b4c2c56c2
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-09-10 08:06:00 +0800

    refactor: don't use PR branch for oedo anymore

--- hosts/nixos/oedo/hardware-configuration.nix
+++ hosts/nixos/oedo/hardware-configuration.nix
@@ -1,7 +1,7 @@
 { inputs, ... }:
 {
   imports = [
-    inputs.nixos-hardware-oedo.nixosModules.dell-precision-5570
+    inputs.nixos-hardware.nixosModules.dell-precision-5570
     inputs.nixos-hardware.nixosModules.common-gpu-nvidia-sync
   ];
 }
