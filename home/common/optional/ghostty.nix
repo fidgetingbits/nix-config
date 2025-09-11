affb507abe43432db4e30635ae7b5420b114753e
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-09-11 11:42:06 +0800

    refactor: remove ghostty fix since it's upstream

--- home/common/optional/ghostty.nix
+++ home/common/optional/ghostty.nix
@@ -1,15 +1,8 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 {
   programs.ghostty = {
     enable = true;
-    package = pkgs.ghostty.overrideAttrs (_: {
-      # https://github.com/NixOS/nixpkgs/issues/421442
-      preBuild = lib.optionalString (lib.versionOlder pkgs.unstable.linux.version "6.15.5") ''
-        shopt -s globstar
-        sed -i 's/^const xev = @import("xev");$/const xev = @import("xev").Epoll;/' **/*.zig
-        shopt -u globstar
-      '';
-    });
+    package = pkgs.unstable.ghostty;
     settings = {
       keybind = [
         # This conflicts with GTK Inspector (needed if ghostty crashes to find gtk info)
