303c739e5d94a39fee6260f6e11e6ee5b2420ab3
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-09-23 21:02:51 +0800

    fix: remove extra / in secrets path

--- scripts/bootstrap-nixos.sh
+++ scripts/bootstrap-nixos.sh
@@ -15,7 +15,7 @@ persist_dir=""
 luks_passphrase="passphrase"
 luks_secondary_drive_labels=""
 git_root=$(git rev-parse --show-toplevel)
-nix_secrets_dir=${NIX_SECRETS_DIR:-"${git_root}"/../nix-secrets/}
+nix_secrets_dir=${NIX_SECRETS_DIR:-"${git_root}"/../nix-secrets}
 shared_secrets=1 # Does host have access to shared.yaml
 
 # Create a temp directory for generated host keys
