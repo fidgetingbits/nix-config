0001663ecca4be014c1a477dc0840a43e7f63a3a
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-09-30 16:40:03 +0800

    chore: bump locks

---
+++ home/common/optional/desktop/hyprland/wlogout.nix
@@ -0,0 +1,52 @@
+{
+  pkgs,
+  ...
+}:
+{
+  programs.wlogout =
+    let
+
+      lockAction = "${pkgs.hyprlock}/bin/hyprlock";
+    in
+    {
+      enable = true;
+      layout = [
+        {
+          label = "lock";
+          action = lockAction;
+          text = "Lock";
+          keybind = "l";
+        }
+        {
+          label = "hibernate";
+          action = "${lockAction} & systemctl hibernate";
+          text = "Hibernate";
+          keybind = "h";
+        }
+        {
+          label = "logout";
+          action = "hyprctl dispatch exit";
+          text = "Logout";
+          keybind = "x";
+        }
+        {
+          label = "shutdown";
+          action = "systemctl poweroff";
+          text = "Shutdown";
+          keybind = "s";
+        }
+        {
+          label = "suspend";
+          action = "${lockAction} & hyprctl dispatch dpms off";
+          text = "Screen Off";
+          keybind = "u";
+        }
+        {
+          label = "reboot";
+          action = "systemctl reboot";
+          text = "Reboot";
+          keybind = "r";
+        }
+      ];
+    };
+}
