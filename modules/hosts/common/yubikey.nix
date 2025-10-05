7216617c0b3fea514d77db00a881f42c9c3e12a1
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-10-05 09:52:00 +0800

    fix(yubikey): buggy conditional and some cleanup

--- modules/hosts/common/yubikey.nix
+++ modules/hosts/common/yubikey.nix
@@ -101,54 +101,37 @@ in
 
       # Yubikey required services and config. See Dr. Duh NixOS config for
       # reference
-      services = lib.optionalAttrs isDarwin {
+      services = lib.optionalAttrs (!isDarwin) {
         yubikey-agent.enable = true;
 
-        udev.extraRules = lib.optionalString pkgs.stdenv.isLinux ''
-          # Link/unlink ssh key on yubikey add/remove
-          SUBSYSTEM=="usb", ACTION=="add", ATTR{idVendor}=="1050", RUN+="${lib.getBin yubikey-up}/bin/yubikey-up"
-          # NOTE: Yubikey 4 has a ID_VENDOR_ID on remove, but not Yubikey 5 BIO, whereas both have a HID_NAME.
-          # Yubikey 5 HID_NAME uses "YubiKey" whereas Yubikey 4 uses "Yubikey", so matching on "Yubi" works for both
-          SUBSYSTEM=="hid", ACTION=="remove", ENV{HID_NAME}=="Yubico Yubi*", RUN+="${lib.getBin yubikey-down}/bin/yubikey-down"
+        udev.extraRules =
+          lib.optionalString pkgs.stdenv.isLinux ''
+            # Link/unlink ssh key on yubikey add/remove
+            SUBSYSTEM=="usb", ACTION=="add", ATTR{idVendor}=="1050", RUN+="${lib.getBin yubikey-up}/bin/yubikey-up"
+            # NOTE: Yubikey 4 has a ID_VENDOR_ID on remove, but not Yubikey 5 BIO, whereas both have a HID_NAME.
+            # Yubikey 5 HID_NAME uses "YubiKey" whereas Yubikey 4 uses "Yubikey", so matching on "Yubi" works for both
+            SUBSYSTEM=="hid", ACTION=="remove", ENV{HID_NAME}=="Yubico Yubi*", RUN+="${lib.getBin yubikey-down}/bin/yubikey-down"
+          ''
+          + lib.optionalString config.yubikey.autoScreenLock ''
+            ##
+            # Yubikey 4
+            ##
 
-          ##
-          # Yubikey 4
-          ##
-
-          # Lock the device if you remove the yubikey (use udevadm monitor -p to debug)
-          # #ENV{ID_MODEL_ID}=="0407", # This doesn't match all the newer keys
-          # FIXME: We only want this to happen if we're undocked, so we need to see how that works. We probably need to run a
-          # script that does smarter checks
-          # ACTION=="remove",\
-          #  ENV{ID_BUS}=="usb",\
-          #  ENV{ID_VENDOR_ID}=="1050",\
-          #  ENV{ID_VENDOR}=="Yubico",\
-          #  RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
-
-          ##
-          # Yubikey 5 BIO
-          #
-          # NOTE: The remove event for the bio doesn't include the ID_VENDOR_ID for some reason, but we can use the
-          # hid name instead. Some HID_NAME might be "Yubico YubiKey OTP+FIDO+CCID" or "Yubico YubiKey FIDO", etc so just
-          # match on "Yubico YubiKey"
-          ##
-
-          # SUBSYSTEM=="hid",\
-          #  ACTION=="remove",\
-          #  ENV{HID_NAME}=="Yubico YubiKey FIDO",\
-          #  RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
-
-          # FIXME: Change this so it only wakes up the screen to the login screen, xset cmd doesn't work
-          # SUBSYSTEM=="hid",\
-          #  ACTION=="add",\
-          #  ENV{HID_NAME}=="Yubico YubiKey FIDO",\
-          #  RUN+="${pkgs.systemd}/bin/loginctl activate 1"
-          #  #RUN+="${lib.getBin pkgs.xorg.xset}/bin/xset dpms force on"
-        '';
+            # Lock the device if you remove the yubikey (use udevadm monitor -p to debug)
+            SUBSYSTEM=="hid", ACTION=="remove", ENV{HID_NAME}=="Yubico Yubi*", RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+          ''
+          + lib.optionalString config.yubikey.autoScreenUnlock ''
+            # FIXME: Change this so it only wakes up the screen to the login screen, xset cmd doesn't work
+            SUBSYSTEM=="hid",\
+             ACTION=="add",\
+             ENV{HID_NAME}=="Yubico YubiKey FIDO",\
+             RUN+="${pkgs.systemd}/bin/loginctl activate 1"
+             #RUN+="${lib.getBin pkgs.xorg.xset}/bin/xset dpms force on"
+          '';
 
         udev.packages = [ pkgs.yubikey-personalization ];
-        pcscd.enable = true;
-      }; # smartcard service
+        pcscd.enable = true; # smartcard service
+      };
 
       # yubikey login / sudo
       security.pam = lib.optionalAttrs pkgs.stdenv.isLinux {
