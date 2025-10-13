d126456427a2daeed799da71e0ce7d2c81ad42b5
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-10-13 09:40:51 -0600

    refactor(zellij): more keybind tweaks and trying to fix tooltip

--- home/common/core/zellij/default.nix
+++ home/common/core/zellij/default.nix
@@ -10,13 +10,17 @@
   };
   # See https://github.com/dj95/zjstatus/discussions/44 for some ricing ideas
   # Use manual file until extraConfig PR is fixed
-  home.file.".config/zellij/config.kdl".source = ./config.kdl;
+  xdg.configFile = {
+    "zellij/config.kdl".source = ./config.kdl;
+    #"zellij/layouts/default.kdl".source = ./default_layout.kdl;
+  };
 
   programs.zsh = {
     shellAliases = {
       zl = "zellij";
       zls = "zellij list-sessions";
       zla = "zellij attach";
+      zld = "zellij action dump-layout"; # Save current layout
     };
     initContent = lib.readFile ./zellij_session_completions;
   };
