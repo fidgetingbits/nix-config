a7bd652605289a7dbad82214a568d37b3eb9d8af
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-10-14 07:45:31 -0600

    feat(rofi-copyq): add rofi-copyq package

--- home/common/optional/desktop/rofi.nix
+++ home/common/optional/desktop/rofi.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }:
 {
+  home.packages = [
+    pkgs.rofi-copyq
+  ];
   programs.rofi = {
     enable = true;
     package = if config.hostSpec.useWayland then pkgs.rofi-wayland else pkgs.rofi;
---
+++ pkgs/common/rofi-copyq/package.nix
@@ -0,0 +1,17 @@
+{ fetchFromGitHub, python311Packages }:
+python311Packages.buildPythonPackage rec {
+  pname = "rofi-copyq";
+  version = "0.1.1";
+
+  src = fetchFromGitHub {
+    owner = "cjbassi";
+    repo = pname;
+    rev = "2ce8628b1e17d91c82d6d40302f1325f3edee207";
+    hash = "sha256-xDxdKitVDonNhoNPMAoHizoaijQj9UQGSCPhWJOcB1w=";
+  };
+
+  pyproject = true;
+  build-system = [ python311Packages.setuptools ];
+
+  patches = [ ./better_type.patch ];
+}
