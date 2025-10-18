c41580a531bf6fb89d5cc1f41cf8e32589d43e9e
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-10-18 05:59:02 -0600

    fix(sshguard): add path to impermanence

--- hosts/common/optional/services/sshguard.nix
+++ hosts/common/optional/services/sshguard.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, config, ... }:
 {
   services.sshguard =
     let
@@ -11,4 +11,20 @@
       detection_time = 8 * hour;
       blacklist_threshold = 30; # 3 strikes
     };
+
+  environment =
+    lib.optionalAttrs (config.system ? impermanence && config.system.impermanence.enable)
+      {
+        persistence = {
+          "${config.hostSpec.persistFolder}".directories = [
+            {
+              # NOTE: systemd Dynamic User requires /var/lib/private to be 0700. See impermanence module
+              directory = "/var/lib/sshguard";
+              #user = "nobody";
+              #group = "nogroup";
+            }
+          ];
+        };
+      };
+
 }
