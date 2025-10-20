d04eb48462a2a2ad9dfff360185a5aacfb80a466
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-10-20 06:04:33 -0600

    fix(ddclient): improve impermanence check

--- hosts/common/optional/services/ddclient.nix
+++ hosts/common/optional/services/ddclient.nix
@@ -12,7 +12,8 @@ in
     enable = true;
     protocol = "gandi";
     zone = config.hostSpec.domain;
-    # NOTE: This record must already exist on gandi in order to update it, otherwise will 404
+    # NOTE: This record must already exist on gandi in order to update it,
+    # otherwise will 404
     domains = [ "${config.hostSpec.hostName}.${config.hostSpec.domain}" ];
     passwordFile = config.sops.secrets."tokens/gandi".path;
     username = "token";
@@ -21,21 +22,23 @@ in
       usev4=webv4
       usev6=disabled
     '';
-
   };
 
-  environment = lib.optionalAttrs config.system.impermanence.enable {
-    persistence = {
-      "${config.hostSpec.persistFolder}".directories = [
-        {
-          # NOTE: systemd Dynamic User requires /var/lib/private to be 0700. See impermanence module
-          directory = "/var/lib/private/ddclient";
-          user = "nobody";
-          group = "nogroup";
-        }
-      ];
-    };
-  };
+  environment =
+    lib.optionalAttrs (config.system ? impermanence && config.system.impermanence.enable)
+      {
+        persistence = {
+          "${config.hostSpec.persistFolder}".directories = [
+            {
+              # NOTE: systemd Dynamic User requires /var/lib/private to be
+              # 0700. See impermanence module
+              directory = "/var/lib/private/ddclient";
+              user = "nobody";
+              group = "nogroup";
+            }
+          ];
+        };
+      };
 
   sops.secrets."tokens/gandi" = {
     sopsFile = "${sopsFolder}/${config.hostSpec.hostName}.yaml";
