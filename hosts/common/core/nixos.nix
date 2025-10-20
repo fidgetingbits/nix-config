b1ad89319adf57a47caaf4de4beef7e1e9793767
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-10-20 06:03:18 -0600

    refactor: remove myth static entry

--- hosts/common/core/nixos.nix
+++ hosts/common/core/nixos.nix
@@ -36,7 +36,6 @@
       # FIXME(networking): oxid IP will be different depending on if we are on it's network or not
       "${network.subnets.oxid.gateway}" = [ "oxid.${config.hostSpec.domain}" ];
       "${network.subnets.ogre.hosts.moon.ip}" = [ "moon.${config.hostSpec.domain}" ];
-      "${network.subnets.ogre.hosts.myth.ip}" = [ "myth.${config.hostSpec.domain}" ];
     }
     // lib.optionalAttrs config.hostSpec.isWork network.work.hosts;
 
