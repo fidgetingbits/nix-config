849ddfa075eb5259491b9c0975b5d956f3f4584a
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-10-22 15:45:48 -0600

    refactor(msmtp): re-add non-postfix relay mail notification

--- hosts/common/optional/msmtp.nix
+++ hosts/common/optional/msmtp.nix
@@ -1,41 +1,71 @@
-{ config, ... }:
+{ config, lib, ... }:
+
+let
+  # FIXME: This should be options probably the the options get set elsewhere eventually
+  #
+  domain = config.hostSpec.domain;
+  notifier = "box@${domain}";
+  useRelay = config.mail-delivery.useRelay;
+  secret = "passwords/${if useRelay then "postfix-relay" else "msmtp"}";
+  host = if useRelay then "mail.${domain}" else "smtp.protonmail.ch";
+  # moon ISP blocks outgoing 25 it seems
+  port = if useRelay then 25 else 587;
+  user = if useRelay then config.hostSpec.hostname else notifier;
+  logfile = "~/.msmtp.log";
+in
 {
-  sops.secrets = {
-    "passwords/postfix-relay" = {
-      owner = config.users.users.${config.hostSpec.username}.name;
-      inherit (config.users.users.${config.hostSpec.username}) group;
+  options.mail-delivery = {
+    useRelay = lib.mkOption {
+      default = false;
+      type = lib.types.bool;
+      description = "Whether or not to use postfix mail relay for mstmp delivery";
     };
   };
 
-  # FIXME: Likely need this on darwin: https://github.com/NixOS/nixpkgs/issues/195532
-  programs.msmtp = {
-    enable = true;
-    setSendmail = true; # set the system sendmail to msmtp's
-
-    accounts =
-      let
-        commonAttrs = {
-          auth = true;
-          tls = true;
-          tls_starttls = true;
-          from = "box@${config.hostSpec.domain}";
-          logfile = "~/.msmtp.log";
-
+  config = {
+    sops.secrets =
+      { }
+      // (lib.mkIf useRelay {
+        "passwords/postfix-relay" = {
+          owner = config.users.users.${config.hostSpec.username}.name;
+          inherit (config.users.users.${config.hostSpec.username}) group;
         };
-      in
-      {
-        "default" = {
-          # FIXME(msmtp): Make this configurable for remotely managed systems
-          host = "mail.${config.hostSpec.domain}";
-          user = config.networking.hostName;
-          passwordeval = "cat ${config.sops.secrets."passwords/postfix-relay".path}";
-        }
-        // commonAttrs;
-        # For cases where postfix relay might be down, like heartbeat-check
-        #"direct" = {
-        #  host = "localhost";
-        #  port = 25;
-        #} // commonAttrs;
-      };
+      })
+      // (lib.mkIf (!useRelay) {
+        "passwords/msmtp" = {
+          owner = config.users.users.${config.hostSpec.username}.name;
+          inherit (config.users.users.${config.hostSpec.username}) group;
+        };
+      });
+
+    # FIXME: Likely need this on darwin: https://github.com/NixOS/nixpkgs/issues/195532
+    programs.msmtp = {
+      enable = true;
+      setSendmail = true; # set the system sendmail to msmtp's
+
+      accounts =
+        let
+          commonAttrs = {
+            inherit user port logfile;
+            auth = true;
+            tls = true;
+            tls_starttls = true;
+            from = notifier;
+          };
+        in
+        {
+          "default" = {
+            inherit host;
+            user = config.networking.hostName;
+            passwordeval = "cat ${config.sops.secrets.${secret}.path}";
+          }
+          // commonAttrs;
+          # For cases where postfix relay might be down, like heartbeat-check
+          #"direct" = {
+          #  host = "localhost";
+          #  port = 25;
+          #} // commonAttrs;
+        };
+    };
   };
 }
