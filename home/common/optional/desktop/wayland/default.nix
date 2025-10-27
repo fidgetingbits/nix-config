3a4d15af883478c31bd3b9fdd0c0d34030b8a967
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-10-27 07:59:35 -0600

    feat(flameshot): enable flameshot, though not working

--- home/common/optional/desktop/wayland/default.nix
+++ home/common/optional/desktop/wayland/default.nix
@@ -1,7 +1,38 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   home.packages = [
     pkgs.wl-clipboard
     pkgs.unstable.grimblast
+    (pkgs.writeShellScriptBin "flameshot-gui" ''
+      export XDG_SESSION_TYPE=x11
+      # export XDG_SESSION_TYPE=wayland
+      # export QT_QPA_PLATFORM=wayland
+      # export QT_AUTO_SCREEN_SCALE_FACTOR=0.5
+      ${lib.getExe' pkgs.flameshot "flameshot"} gui
+    '')
   ];
+
+  # services.flameshot = {
+  #   enable = true;
+  #   package = pkgs.unstable.flameshot;
+  #   settings = {
+  #     General = {
+  #       useGrimAdapter = true;
+  #       disabledTrayIcon = false;
+  #       showStartupLaunchMessage = true;
+  #       showAbortNotification = true;
+  #       historyConfirmationToDelete = false;
+  #       showHelp = false;
+  #       showMagnifier = true;
+  #       showSidePanelButton = false;
+  #       #        uiColor = "#0f111b";
+  #       #        drawColor = "#D81E5B";
+  #       drawThickness = 4;
+  #
+  #       # Save/Export.
+  #       copyPathAfterSave = true;
+  #     };
+  #   };
+  # };
+
 }
