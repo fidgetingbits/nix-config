de7585b733d0ca845b535228ca919276dd1a1431
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-11-05 08:33:57 -0700

    docs(virt): comments for virtual networks

--- hosts/common/optional/libvirt.nix
+++ hosts/common/optional/libvirt.nix
@@ -80,6 +80,7 @@ in
                     start = "${subnet.triplet}.100";
                     end = "${subnet.triplet}.254";
                   };
+                  # Static IP addresses
                   host = lib.attrValues subnet.hosts;
                 };
               };
@@ -92,30 +93,29 @@ in
   # Need to add [File (in the menu bar) -> Add connection] when start for the first time
   programs.virt-manager.enable = true;
 
-  environment =
-    {
-      systemPackages = [
-        # QEMU/KVM(HostCpuOnly), provides:
-        #   qemu-storage-daemon qemu-edid qemu-ga
-        #   qemu-pr-helper qemu-nbd elf2dmp qemu-img qemu-io
-        #   qemu-kvm qemu-system-x86_64 qemu-system-aarch64 qemu-system-i386
-        pkgs.qemu_kvm
+  environment = {
+    systemPackages = [
+      # QEMU/KVM(HostCpuOnly), provides:
+      #   qemu-storage-daemon qemu-edid qemu-ga
+      #   qemu-pr-helper qemu-nbd elf2dmp qemu-img qemu-io
+      #   qemu-kvm qemu-system-x86_64 qemu-system-aarch64 qemu-system-i386
+      pkgs.qemu_kvm
 
-        # Install QEMU(other architectures), provides:
-        #   ......
-        #   qemu-loongarch64 qemu-system-loongarch64
-        #   qemu-riscv64 qemu-system-riscv64 qemu-riscv32  qemu-system-riscv32
-        #   qemu-system-arm qemu-arm qemu-armeb qemu-system-aarch64 qemu-aarch64 qemu-aarch64_be
-        #   qemu-system-xtensa qemu-xtensa qemu-system-xtensaeb qemu-xtensaeb
-        #   ......
-        pkgs.qemu
-      ];
-    }
-    // lib.optionalAttrs config.system.impermanence.enable {
-      persistence = {
-        "${config.hostSpec.persistFolder}".directories = [ "/var/lib/libvirt" ];
-      };
+      # Install QEMU(other architectures), provides:
+      #   ......
+      #   qemu-loongarch64 qemu-system-loongarch64
+      #   qemu-riscv64 qemu-system-riscv64 qemu-riscv32  qemu-system-riscv32
+      #   qemu-system-arm qemu-arm qemu-armeb qemu-system-aarch64 qemu-aarch64 qemu-aarch64_be
+      #   qemu-system-xtensa qemu-xtensa qemu-system-xtensaeb qemu-xtensaeb
+      #   ......
+      pkgs.qemu
+    ];
+  }
+  // lib.optionalAttrs config.system.impermanence.enable {
+    persistence = {
+      "${config.hostSpec.persistFolder}".directories = [ "/var/lib/libvirt" ];
     };
+  };
 
   users.users.${config.hostSpec.username} = {
     extraGroups = [ "libvirtd" ];
