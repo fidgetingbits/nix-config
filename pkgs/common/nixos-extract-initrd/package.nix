ffb4f4f758f3a99f19969192956e1fa13d50470a
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-11-16 09:27:03 -0700

    feat: add script for extracting nix initrd images

---
+++ pkgs/common/nixos-extract-initrd/package.nix
@@ -0,0 +1,10 @@
+{ stdenv, pkgs, ... }:
+# https://gist.githubusercontent.com/pshirshov/2b5744abbcfae376a87a80bc501a0866/raw/2f15ff5c2715275e42d22691b8eca2969bbc85cf/nixos-extract-initrd.py
+stdenv.mkDerivation {
+  name = "nixos-extract-initrd";
+  buildInputs = [ (pkgs.python3.withPackages (pythonPackages: builtins.attrValues { })) ];
+  dontUnpack = true;
+  installPhase = ''
+    install -Dm755 ${./nixos-extract-initrd.py} $out/bin/nixos-extract-initrd
+  '';
+}
