74e5e136eabb57ef37f2ddb8f11462766f4b3490
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-12-03 11:55:45 +0800

    refactor(xrdp): comment out conflicts, though doesn't work anyway

--- hosts/common/optional/services/xrdp.nix
+++ hosts/common/optional/services/xrdp.nix
@@ -7,7 +7,7 @@
   services.xrdp.enable = true;
   services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session"; # gnome wayland session
   services.gnome.gnome-remote-desktop.enable = true;
-  services.displayManager.autoLogin.enable = false;
-  services.getty.autologinUser = null;
+  #services.displayManager.autoLogin.enable = false;
+  #services.getty.autologinUser = null;
   networking.firewall.allowedTCPPorts = [ 3389 ];
 }
