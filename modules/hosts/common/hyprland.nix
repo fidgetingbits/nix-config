a7d46098e8eb660bc95b09d1f28ee9d887640668
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-12-06 10:04:57 +0800

    fix(hyprland): use an enable

--- modules/hosts/common/hyprland.nix
+++ modules/hosts/common/hyprland.nix
@@ -1,11 +1,22 @@
-{ pkgs, ... }:
 {
-  programs.hyprland = {
-    package = pkgs.unstable.hyprland;
-    enable = true;
-    withUWSM = true; # systemd management of hyprland
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  cfg = config.desktops.hyprland;
+in
+{
+  options = {
+    desktops.hyprland.enable = lib.mkEnableOption "Enable hyprland and related functionality";
   };
 
-  environment.systemPackages = [
-  ];
+  config = lib.mkIf cfg.enable {
+    programs.hyprland = {
+      package = pkgs.unstable.hyprland;
+      enable = true;
+      withUWSM = true; # systemd management of hyprland
+    };
+  };
 }
