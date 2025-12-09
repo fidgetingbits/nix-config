072f72999061ec09cbbda7052c54ab7bcd15d739
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-12-09 21:51:38 +0800

    feat(oppo): define monitor

--- hosts/nixos/oppo/monitors.nix
+++ hosts/nixos/oppo/monitors.nix
@@ -1,4 +1,13 @@
 { ... }:
 {
-
+  # LG Electronics LG TV SSCR2
+  monitors = {
+    "HDMI-1" = {
+      width = 3840;
+      height = 2160;
+      refreshRate = 120.00;
+      scale = 2.0;
+      primary = true;
+    };
+  };
 }
