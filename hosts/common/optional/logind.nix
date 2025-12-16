f36a9d88b8a4263466b96af4926ecdf270aa4b7b
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-12-16 18:16:40 +0800

    feat(logind): add some debug tools

--- hosts/common/optional/logind.nix
+++ hosts/common/optional/logind.nix
@@ -1,5 +1,10 @@
-{ ... }:
+# https://github.com/asus-linux-drivers/asus-fliplock-driver
+{ pkgs, ... }:
 {
+  environment.systemPackages = [
+    pkgs.libinput # debug events
+    pkgs.acpid # debug acpi events
+  ];
   services.acpid.enable = true;
   services.logind = {
     settings.Login = {
