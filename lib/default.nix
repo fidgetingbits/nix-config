78f2396840c9c82f2b9259b1c12b4d15b62bfee6
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-12-16 18:16:23 +0800

    refactor(lib): fix scanPaths to not import a dir that doesn't have a default.nix

--- lib/default.nix
+++ lib/default.nix
@@ -1,26 +1,25 @@
 { lib, ... }:
 rec {
-  # use path relative to the root of the project
-  relativeToRoot = lib.path.append ../.;
+  # sub libs
   time = import ./time.nix;
 
-  # Imports any .nix file in the specific directory, and any folder. Note this
-  # means that a folder containing `default.nix` and other *.nix files is expected
-  # to use the other *.nix files in that folder as supplementary, and not distinct
-  # modules
+  # use path relative to the root of the project
+  relativeToRoot = lib.path.append ../.;
+
+  # Imports any .nix file in the specific directory, and any folder that
+  # contains a default.nix. Note this means that a folder containing
+  # `default.nix` and other *.nix files is expected to use the other *.nix
+  # files in that folder as supplementary, and not distinct modules
   scanPaths =
     path:
     lib.map (f: (path + "/${f}")) (
-      lib.attrNames (
-        lib.attrsets.filterAttrs (
-          path: _type:
-          (_type == "directory") # include directories
-          || (
-            (path != "default.nix") # ignore default.nix
-            && (lib.strings.hasSuffix ".nix" path) # include .nix files
-          )
-        ) (builtins.readDir path)
+      builtins.readDir path
+      |> lib.attrsets.filterAttrs (
+        file: _type:
+        (_type == "directory" && lib.pathExists (path + "/${file}/default.nix"))
+        || (file != "default.nix" && lib.strings.hasSuffix ".nix" file)
       )
+      |> lib.attrNames
     );
 
   leaf = str: lib.last (lib.splitString "/" str);
