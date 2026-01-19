1045692678b98a79fac26bfca185d8261fc462a9
fidgetingbits <fidgetingbits@users.noreply.github.com>
2026-01-19 16:19:02 +0800

    fix(ossa): use correct monitor specs

--- hosts/nixos/ossa/monitors.nix
+++ hosts/nixos/ossa/monitors.nix
@@ -4,11 +4,11 @@
 {
   monitors = {
     "eDP-1" = {
-      width = 3840;
-      height = 2160;
-      refreshRate = 60.00;
+      width = 2560;
+      height = 1600;
+      refreshRate = 165.00;
       #transform = 2;
-      scale = 2.0;
+      # scale = 2.0;
     };
   };
 }
