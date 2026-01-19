c241e8cd0648ddfbc49000361574fadda0d520fa
fidgetingbits <fidgetingbits@users.noreply.github.com>
2026-01-19 14:22:53 +0800

    refactor: re-enable non-hyprland cursor

--- modules/home/cursor.nix
+++ modules/home/cursor.nix
@@ -1,5 +1,5 @@
 {
-  # pkgs,
+  pkgs,
   lib,
   osConfig,
   ...
@@ -8,19 +8,19 @@ lib.mkIf osConfig.hostSpec.isAutoStyled {
   # FIXME: This is busted
   #error (ignored): The option `home-manager.users.aa.stylix' does not exist. Definition values:
   #- In `/nix/store/2qhywi14paiw3s90229mhbrkpizjp8d7-source/modules/home/cursor.nix':
-  #  stylix = {
-  #    cursor = (
-  #      lib.mkOverride 200 {
-  #        package = pkgs.rose-pine-cursor;
-  #        name = "BreezeX-RosePine-Linux";
-  #        size = 30;
-  #      }
-  #    );
+  stylix = {
+    # cursor = (
+    #   lib.mkOverride 200 {
+    #     package = pkgs.rose-pine-cursor;
+    #     name = "BreezeX-RosePine-Linux";
+    #     size = 30;
+    #   }
+    # );
 
-  # cursor = lib.mkForce {
-  #   name = "Breeze_Hacked";
-  #   package = pkgs.breeze-hacked-cursor-theme;
-  #   size = 30;
-  # };
-  # };
+    cursor = lib.mkForce {
+      name = "Breeze_Hacked";
+      package = pkgs.breeze-hacked-cursor-theme;
+      size = 30;
+    };
+  };
 }
