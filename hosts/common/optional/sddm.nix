eb4a756285b3c95417af07a0a1710a5a00c1cabe
fidgetingbits <fidgetingbits@users.noreply.github.com>
2026-02-05 18:14:52 +0800

    docs(sddm): remove breakage note

--- hosts/common/optional/sddm.nix
+++ hosts/common/optional/sddm.nix
@@ -11,16 +11,15 @@ let
   };
 in
 {
-  #  imports = [
-  #    ./x11.nix # We use X11 because wayland breaks the theme animations
-  #  ];
+  #imports = [
+  #  ./x11.nix # We use X11 because wayland breaks the theme animations
+  #];
   security.pam.services.sddm.enableGnomeKeyring = true;
   environment.systemPackages = [ sddm-theme ];
   qt.enable = true;
   services.displayManager.sddm = {
     package = pkgs.kdePackages.sddm; # use qt6 version of sddm
     enable = true;
-    # NOTE: This breaks the video wallpaper from the silentSDDM theme...
     wayland.enable = config.hostSpec.useWayland;
     theme = sddm-theme.pname;
     extraPackages = sddm-theme.propagatedBuildInputs;
