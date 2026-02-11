12a1a203bc24b1d4a1c5964ff9493ae5a2955a77
fidgetingbits <fidgetingbits@users.noreply.github.com>
2026-02-11 16:37:54 +0800

    feat(monit): add disk monitoring

--- modules/hosts/nixos/monit.nix
+++ modules/hosts/nixos/monit.nix
@@ -1,3 +1,8 @@
+# Some inspiration from:
+#  - https://github.com/tiredofit/nix-modules/blob/5cb4be/nixos/service/monit.nix
+#
+# To read:
+#  - https://github.com/filipelsilva/nixos-config/blob/be22e8c1/modules/monit.nix
 {
   config,
   lib,
@@ -14,14 +19,14 @@ let
     config.mail-delivery;
   monitConfig = ''
     set daemon 30
-    ${lib.concatMapStringsSep "\n" (a: "set alert ${a}") cfg.notify.to}
+    ${lib.concatMapStringsSep "\n" (a: "set alert ${a} but not on { instance }") cfg.settings.email.to}
     set logfile /var/log/monit.log
 
     include ${config.sops.templates.${secretConfig}.path}
 
     set mail-format {
-     from: ${cfg.notify.from}
-     subject: [$HOST: $SERVICE]: $EVENT on $DATE
+     from: ${cfg.settings.email.from}
+     subject: [$HOST: monit]: $EVENT on $DATE
      message: $DESCRIPTION
     }
   '';
@@ -35,32 +40,88 @@ in
         example = true;
         description = "Enables the monit service for monitoring and alerting on system resources";
       };
-      notify = lib.mkOption {
-        type = lib.types.submodule {
-          options = {
-            to = lib.mkOption {
-              type = lib.types.listOf lib.types.str;
-              default = [ config.hostSpec.email.admin ];
-              example = [ "admin@example.com" ];
-              description = "List of emails to send UPS notifications to";
-            };
-            from = lib.mkOption {
-              type = lib.types.str;
-              default = config.hostSpec.email.notifier;
-              example = "notifications@example.com";
-              description = "Email address to send UPS notifications from";
-            };
-          };
-
+      settings = {
+        checkInterval = lib.mkOption {
+          default = "30";
+          type = lib.types.str;
+          description = "Check services at however many second intervals";
         };
-        default = { };
+        email = lib.mkOption {
+          type = lib.types.submodule {
+            options = {
+              to = lib.mkOption {
+                type = lib.types.listOf lib.types.str;
+                default = [ config.hostSpec.email.admin ];
+                example = [ "admin@example.com" ];
+                description = "List of emails to send UPS notifications to";
+              };
+              from = lib.mkOption {
+                type = lib.types.str;
+                default = config.hostSpec.email.notifier;
+                example = "notifications@example.com";
+                description = "Email address to send UPS notifications from";
+              };
+            };
+
+          };
+          default = { };
+        };
+      };
+      usage = {
+        filesystem = {
+          enable = lib.mkOption {
+            default = true;
+            type = lib.types.bool;
+            description = "Enables filesystem usage monitoring";
+          };
+          disks = lib.mkOption {
+            type = lib.types.attrsOf (
+              lib.types.submodule {
+                options = {
+                  limit = lib.mkOption {
+                    default = "90%";
+                    type = lib.types.str;
+                    description = "Limit for usage monitoring";
+                  };
+                  path = lib.mkOption {
+                    default = "/";
+                    type = lib.types.str;
+                    description = "Path to monitor";
+                  };
+                };
+              }
+            );
+            # single list entry with the default submodule values
+            default = {
+              rootfs = { };
+            };
+            description = "List of disks to monitor";
+          };
+        };
       };
     };
   };
   config = lib.mkIf cfg.enable {
     services.monit = {
       enable = true;
-      config = monitConfig;
+      config =
+        let
+          monitDisks =
+            if cfg.usage.filesystem.enable then
+              let
+                disks = cfg.usage.filesystem.disks;
+              in
+              lib.concatMapStringsSep "\n" (name: ''
+                check filesystem ${name} with path ${disks.${name}.path}
+                  if space usage > ${disks.${name}.limit} then alert
+              '') (lib.attrNames disks)
+            else
+              "";
+        in
+        lib.concatStringsSep "\n" [
+          monitConfig
+          monitDisks
+        ];
     };
     sops.templates.${secretConfig} =
       let
