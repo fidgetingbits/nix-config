8c9dd6e5ec20a944836fe66227cf0afb4cd17920
fidgetingbits <fidgetingbits@users.noreply.github.com>
2026-03-06 21:42:38 +0800

    refactor: clean up neovim terminal stuff

--- home/common/core/neovim.nix
+++ home/common/core/neovim.nix
@@ -41,13 +41,6 @@
       # This is so we don't spawn embedded neovim if already in a neovim terminal
       # FIXME: Maybe look into vimception plugin to fix this eventually
       # neovim = "${pkgs.introdus.neovim-wrapped}/bin/neovim.sh";
-      # FIXME: Anything with -<command> is broken because ultimately it goes through neovim-openfile, which won't
-      # handle the cmdline arguments properly
-      ex = "nvim -e";
-      rview = "nvim -RZ";
-      rvim = "nvim -Z";
-      view = "nvim -R";
-      vimdiff = "nvim -d";
     };
   };
 }
--- pkgs/neovim-term/package.nix
+++ pkgs/neovim-term/package.nix
@@ -1,9 +1,14 @@
-{ pkgs }:
+{
+  pkgs,
+  lib,
+  config,
+}:
 pkgs.writeShellScriptBin "neovim-term.sh" ''
   #!/usr/bin/env bash
   # NOTE: Don't use -u as NVIM is set from outside the script
   set -eo pipefail
 
   # IMPORTANT: talon title handling relies on the explicit zsh
-  ${pkgs.fidgetingvim}/bin/nvim -c ':term zsh'
+  ${lib.getExe config.wrappers.neovim.wrapper} -c ':term zsh'
+
 ''
