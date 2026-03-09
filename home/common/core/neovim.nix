559a27aedacf16adb46a3059e65d56a13ce084f9
fidgetingbits <fidgetingbits@users.noreply.github.com>
2026-03-09 16:43:46 +0800

    feat: enable terminal mode setting and fix font

--- home/common/core/neovim.nix
+++ home/common/core/neovim.nix
@@ -1,10 +1,35 @@
 {
   inputs,
   lib,
-  # pkgs,
+  pkgs,
   osConfig,
+  config,
   ...
 }:
+let
+  # From github, but useful for nested terminals. Tie to nvim terminal and add
+  # https://github.com/sephid86/nixos/blob/5eefc2eb3c3f2afc362235682176f42916c8b948/homenix/home.nix#L362
+  nvim-add = pkgs.writeShellApplication {
+    name = "nvim-add";
+    runtimeInputs = [ config.wrappers.neovim.wrapper ];
+    text = ''
+      #!/usr/bin/env bash
+      NVIM_SOCKET="$HOME/.cache/nvim.sock"
+      # [ "$#" -gt 0 ] && set -- $(realpath -- "$@")
+      if [ "$#" -gt 0 ]; then
+          mapfile -t real_files < <(realpath -- "$@")
+          set -- "''${real_files[@]}"
+      fi
+      if n_=$(nvim --headless --server "$NVIM_SOCKET" --remote-expr "1" 2>/dev/null); then
+      if [ "$#" -eq 0 ]; then set -- "unnamed"; fi
+        exec nvim --server "$NVIM_SOCKET" --remote "$@"
+      else
+        rm -f $NVIM_SOCKET
+        exec nvim --listen "$NVIM_SOCKET" "$@"
+      fi
+    '';
+  };
+in
 {
   imports = [
     (inputs.wrappers.lib.mkInstallModule {
@@ -28,7 +53,15 @@
         # NOTE: This means you need the neovim source at the specified unwrapped_config path
         # ex ~/dev/nix/neovim
         devMode = isDev;
-        terminal = osConfig.hostSpec.useNeovimTerminal;
+        terminalMode = osConfig.hostSpec.useNeovimTerminal;
+        guifont =
+          let
+            fontSize = 12; # FIXME: This should come from somewhere else
+          in
+          # FIXME: Doesn't seem to work when setting all fonts, so just defaulting to first for now
+          [ (lib.head osConfig.fonts.fontconfig.defaultFonts.monospace) ]
+          |> map (f: "${f}:h${toString fontSize}")
+          |> lib.concatStringsSep ",";
       };
     };
 
@@ -43,4 +76,5 @@
       # neovim = "${pkgs.introdus.neovim-wrapped}/bin/neovim.sh";
     };
   };
+  home.packages = [ nvim-add ];
 }
