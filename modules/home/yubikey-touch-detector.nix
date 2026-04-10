4173d228e291c63d1bf196e3c6c407ef9490b962
fidgetingbits <fidgetingbits@users.noreply.github.com>
2026-04-10 08:22:25 +0800

    refactor(yubikey): cleanup touch detector

--- modules/home/yubikey-touch-detector.nix
+++ modules/home/yubikey-touch-detector.nix
@@ -1,5 +1,4 @@
-# See https://github.com/berbiche/dotfiles/blob/4048a1746ccfbf7b96fe734596981d2a1d857930/modules/home-manager/yubikey-touch-detector.nix#L9
-# FIXME: Send a PR to HM to add this service
+# Adapted from https://github.com/berbiche/dotfiles/blob/4048a1746ccfbf7b96fe734596981d2a1d857930/modules/home-manager/yubikey-touch-detector.nix#L9
 {
   inputs,
   config,
@@ -12,7 +11,7 @@ let
 in
 {
   options.services.yubikey-touch-detector = {
-    enable = lib.mkEnableOption "a tool to detect when your YubiKey is waiting for a touch";
+    enable = lib.mkEnableOption "A tool to detect when your YubiKey is waiting for a touch";
 
     extraArgs = lib.mkOption {
       type = lib.types.listOf lib.types.str;
@@ -31,7 +30,6 @@ in
     };
     notificationSoundFile = lib.mkOption {
       type = lib.types.str;
-      #default = "${pkgs.sound-theme-freedesktop}/share/sounds/freedesktop/stereo/window-attention.oga";
       default = "${inputs.nix-assets}/notifications/john_spartan.ogg";
       description = ''
         Path to the sound file to play when the YubiKey is waiting for a touch.
@@ -39,19 +37,15 @@ in
     };
   };
 
-  # NOTE: I duplicate the services and stuff here because it doesn't exist for
-  # home-manager, only for programs.yubikey-touch-detector in nixos.
   config = lib.mkIf cfg.enable {
-    #programs.yubikey-touch-detector.enable = true;
     home.packages = [ pkgs.yubikey-touch-detector ];
 
-    # Same license thing for the description here
     systemd.user.services.yubikey-touch-detector = {
       Unit = {
         Description = "Detects when your YubiKey is waiting for a touch";
       };
       Service = {
-        ExecStart = "${lib.getExe' pkgs.yubikey-touch-detector "yubikey-touch-detector"} ${lib.concatStringsSep " " cfg.extraArgs}";
+        ExecStart = "${lib.getExe pkgs.yubikey-touch-detector} ${lib.concatStringsSep " " cfg.extraArgs}";
         Environment = [ "PATH=${lib.makeBinPath [ pkgs.gnupg ]}" ];
         Restart = "on-failure";
         RestartSec = "1sec";
