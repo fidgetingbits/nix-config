737440aad952eb6bd526279fd7843de298920a21
fidgetingbits <fidgetingbits@users.noreply.github.com>
2026-04-10 11:03:54 +0800

    feat: support non-dev neovim wrapper setup

--- home/common/core/neovim.nix
+++ home/common/core/neovim.nix
@@ -1,5 +1,7 @@
 {
   lib,
+  inputs,
+  osConfig,
   ...
 }:
 {
@@ -11,10 +13,17 @@
   # My custom neovim wrapper, built on top of the introdus neovim base, is enabled by the above
   # and exposed in the config as wrappers.neovim.
   wrappers.neovim = {
-    settings = {
-      # Set impure paths to allow hot reloading of `plugin/`, `snippets/`, etc
-      unwrappedConfig = "/home/aa/dev/nix/neovim";
-      baseConfig = lib.mkForce "/home/aa/dev/nix/introdus/aa/wrappers/neovim";
-    };
+    settings =
+      if osConfig.hostSpec.isDevelopment then
+        {
+          # Set impure paths to allow hot reloading of `plugin/`, `snippets/`, etc
+          unwrappedConfig = "/home/aa/dev/nix/neovim";
+          baseConfig = lib.mkForce "/home/aa/dev/nix/introdus/aa/wrappers/neovim";
+        }
+      else
+        {
+          # Non-development boxes just use whatever is already in git
+          baseConfig = lib.mkForce "${inputs.introdus}/wrappers/neovim";
+        };
   };
 }
