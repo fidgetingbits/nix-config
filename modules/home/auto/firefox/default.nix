c787264df2ade74349dd09f3f0a2f30bdcf05472
fidgetingbits <fidgetingbits@users.noreply.github.com>
2026-05-04 15:31:50 +0800

    feat(tridactyl): more settings/binds

--- modules/home/auto/firefox/default.nix
+++ modules/home/auto/firefox/default.nix
@@ -12,11 +12,14 @@ lib.mkIf config.programs.firefox.enable {
     search = [ (import ./search.nix { inherit lib pkgs; }) ];
   };
 
-  # See https://github.com/DivitMittal/firefox-nixCfg/blob/dc55e3750eac4d6381301901e269106efae621ff/config/tridactyl/config/tridactylrc
-  # for more examples (and tridactyl repo)
+  # FIXME: This should be tied to an enabled, but for now you have to manually
+  # enable it in firefox anyway so won't affect anyone that isn't using it
+  # anyway
+  #
+  # Cheatsheet: https://cheatography.com/goumlechat/cheat-sheets/tridactyl/
   xdg.configFile."tridactyl/tridactylrc".text =
     let
-      # FIXME: Make this an option elsehwere
+      # FIXME: Make this an option elsewhere
       # theme = "catppuccin-${config.catppuccin.flavor}";
       theme = "catppuccin-mocha";
     in
@@ -30,6 +33,8 @@ lib.mkIf config.programs.firefox.enable {
 
       " Don't use uppercase key for hint, since you don't press shift+key
       set hintuppercase false
+      " Don't accidentally close pinned tabs
+      set tabclosepinned false
 
       " Prevent changing the color elements. Only show the key hint
       " From here: https://github.com/jrolfs/gruvbox-material-tridactyl/issues/6
@@ -42,6 +47,19 @@ lib.mkIf config.programs.firefox.enable {
       " Adjust keybinds to account for vertical tabs
       bind --mode=normal J tabnext
       bind --mode=normal K tabprev
+      " Same as vim
+      bind n findnext 1
+      bind N findnext 1 -?
+
+      bind gd tabdetach
+      " Handy multiwindow/multitasking binds
+      bind gD composite tabduplicate; tabdetach
+
+      " FIXME: Add some of my search aliases
+      " bind c fillcmdline_notrail tabopen @nix
+
+      " FIXME: add quickmarks
+      " quickmark g https://mail.google.com/mail/u/0/#inbox
 
       " WARNING: This is probably a bad idea, as it disables most clickables, but
       " otherwise it highlights everything. Below doesn't work with image links
