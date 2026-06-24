8fb589c93e715fca0083c6d29d8d258114e648b0
fidgetingbits <fidgetingbits@users.noreply.github.com>
2026-06-24 08:40:09 +0800

    feat: add lexmark driver for printing

--- hosts/common/optional/printing.nix
+++ hosts/common/optional/printing.nix
@@ -1,10 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [
+      postscript-lexmark
+      lexmark-aex
+    ];
+  };
+
   # https://discourse.nixos.org/t/newly-announced-vulnerabilities-in-cups/52771
   #services.printing.browsed.enable = false;
+  services.ipp-usb.enable = true;
 
+  # For network printer discovery
   services.avahi = {
     enable = true;
     nssmdns4 = true;
